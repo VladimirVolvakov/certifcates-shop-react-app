6c42e2fa7db6ecd4e3fe688f99fef6480249fe28
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-04 23:50:22 +0300

    style: add styles for Impressions component

--- src/components/Impressions.styles.js
+++ src/components/Impressions.styles.js
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 
-export const CatalogueContainer = styled.div``;
+export const CatalogueContainer = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 100px 0;
+`;
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+  font-size: 40px;
+  line-height: 50px;
+  weight: 700;
+  color: #333;
+  text-align: center;
+  margin-bottom: 60px;
+`;
 
-export const Catalogue = styled.div``;
+export const Catalogue = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 40px;
+`;
