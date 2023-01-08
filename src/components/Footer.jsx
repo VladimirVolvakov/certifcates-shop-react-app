23ffd58afc4b62a9987885dc58e2cc90fe879152
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-08 15:31:43 +0300

    style: add styles for Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 // Styles:
-import { Button, CallUs, ContactsContainer, Container, Image, Logo, LogoContainer, Menu, MenuContainer, MenuHeader, MenuItem, PhoneNumber, RequestContainer, SocialMedia, Text, Title } from './Footer.styles';
+import { Button, CallUs, ContactsContainer, Container, Image, Logo, LogoContainer, Menu, MenuContainer, MenuHeader, MenuItem, RequestContainer, SocialMedia, Text, Title } from './Footer.styles';
 // Images:
 import InstaIcon from '../assets/icons/InstagramIcon.png';
-import LogoImage from '../assets/icons/Group_54.png';
-import LogoText from '../assets/icons/Group.png';
+import LogoImage from '../assets/icons/LogoImage.png';
+import LogoText from '../assets/icons/LogoText.png';
 import VkIcon from '../assets/icons/VkIcon.png';
 
 const Footer = () => {
@@ -26,7 +26,7 @@ const Footer = () => {
         </Menu>
       </MenuContainer>
       <ContactsContainer>
-        <PhoneNumber>+38-050-123-45-67</PhoneNumber>
+        <Title>+38-050-123-45-67</Title>
         <SocialMedia>
           <Image src={VkIcon} />
           <Image src={InstaIcon} />
@@ -34,7 +34,7 @@ const Footer = () => {
       </ContactsContainer>
       <RequestContainer>
         <Title>Остались вопросы?</Title>
-        <CallUs>Оставьте заявку, мы свяжемся с Вами в ближайшее время</CallUs>
+        <CallUs>Оставьте заявку, мы свяжемся с Вами <br/> в ближайшее время</CallUs>
         <Button>Оставить заявку</Button>
       </RequestContainer>
     </Container>
--- src/components/Footer.styles.js
+++ src/components/Footer.styles.js
@@ -1,33 +1,114 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 285px;
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding-top: 60px;
+  border-top: 1px solid #547147;
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const LogoContainer = styled.div``;
 
-export const Logo = styled.div``;
+export const Logo = styled.div`
+  width: 261px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 29px;
+`;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  cursor: pointer;
 
-export const Text = styled.p``;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  line-height: 18px;
+  font-weight: 400;
+  color: #888;
+`;
 
 export const MenuContainer = styled.div``;
 
-export const MenuHeader = styled.h3``;
+export const MenuHeader = styled.h3`
+  font-size: 24px;
+  line-height: 28px;
+  font-weight: 600;
+  color: #333;
+  margin-bottom: 20px;
+`;
 
-export const Menu = styled.div``;
+export const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
-export const MenuItem = styled.h4``;
+export const MenuItem = styled.p`
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 600;
+  color: #333;
+`;
 
-export const ContactsContainer = styled.div``;
+export const ContactsContainer = styled.div`
+  height: 94px;
+  width: 280px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-export const PhoneNumber = styled.p``;
+export const SocialMedia = styled.div`
+  width: 100px;
+  display: flex;
+  justify-content: space-between;
+`;
 
-export const SocialMedia = styled.div``;
+export const RequestContainer = styled.div`
+  width: 420px;
+`;
 
-export const RequestContainer = styled.div``;
+export const Title = styled.h2`
+  font-size: 30px;
+  line-height: 34px;
+  font-weight: 600;
+  color: #333;
+`;
 
-export const Title = styled.h3``;
+export const CallUs = styled.p`
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #333;
+  margin: 15px 0 20px;
+`;
 
-export const CallUs = styled.p``;
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 32px;
+  border-radius: 40px;
+  border: none;
+  background: #547147;
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s;
 
-export const Button = styled.button``;
+  &:hover {
+    background: #547900;
+    opacity: 0.8;
+  }
+`;
