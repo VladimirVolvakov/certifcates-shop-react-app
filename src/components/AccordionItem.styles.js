b973a8ae8bcfa3701a54aa15273a7ebcb0fc4825
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-17 23:53:32 +0300

    style: add styles for AccordionFaq & AccordionItem components

--- src/components/AccordionItem.styles.js
+++ src/components/AccordionItem.styles.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
-export const AccordionItm = styled.div``;
+export const AccordionItm = styled.div`
+  border-bottom: 2px solid #eaebef;
+`;
 
-export const AccordionHeader = styled.div``;
+export const AccordionHeader = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const AccordionBody = styled.div``;
 
@@ -11,15 +19,32 @@ export const Question = styled.h3`
   line-height: 37px;
   font-weight: 600;
   color: #333;
-  width: 100%;
-  border: none;
 `;
 
-export const Toggler = styled.div``;
+export const Toggler = styled.div`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background: #e8e8e8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: #333;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    color: #e8e8e8;
+    background: #333;
+  }
+`;
 
 export const Answer = styled.div`
-  font-size: 16px;
-  line-height: 20px;
+  font-size: 20px;
+  line-height: 24px;
   font-weight: 400;
   color: #333;
+  margin-bottom: 25px;
 `;
