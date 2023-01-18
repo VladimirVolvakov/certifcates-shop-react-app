88ce39decd2f6ef417158d623ff9f685e51d79d9
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-18 18:20:03 +0300

    feat: add markup & styles for Modal component

---
+++ src/components/ui/Modal.jsx
@@ -0,0 +1,12 @@
+// Styles: 
+import { Container, Wrapper } from './Modal.styles';
+
+const Modal = ({ children }) => {
+  <Container>
+    <Wrapper>
+      { children }
+    </Wrapper>
+  </Container>
+};
+
+export default Modal;
