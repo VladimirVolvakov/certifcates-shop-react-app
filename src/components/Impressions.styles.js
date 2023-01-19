31f9c2ec9d537efc75cadeb3f6eeb5a8b4d8be8e
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-19 13:33:48 +0300

    style: add styles for media query < 375 px for Impressions component

--- src/components/Impressions.styles.js
+++ src/components/Impressions.styles.js
@@ -3,8 +3,12 @@ import styled from "styled-components";
 export const CatalogueContainer = styled.div`
   width: 100%;
   max-width: 1300px;
-  margin: 0 auto;
-  padding: 100px 0;
+  margin: 100px auto;
+
+  @media (max-width: 375px) {
+    max-width: 335px;
+    margin: 60px auto;
+  }
 `;
 
 export const Title = styled.h2`
@@ -14,10 +18,21 @@ export const Title = styled.h2`
   color: #333;
   text-align: center;
   margin-bottom: 60px;
+
+  @media (max-width: 375px) {
+    font-size: 21px;
+    line-height: 25px;
+    margin-bottom: 30px;
+  }
 `;
 
 export const Catalogue = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 40px;
+
+  @media (max-width: 375px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 19px;
+  }
 `;
