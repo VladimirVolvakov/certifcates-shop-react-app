1a7728c7a0c36bfe32eda2534bc5eb78c3336464
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-19 13:13:43 +0300

    style: add styles for media query < 375 px for Advantages & AdvantageItem components

--- src/components/AdvantageItem.styles.js
+++ src/components/AdvantageItem.styles.js
@@ -5,10 +5,19 @@ export const ItemContainer = styled.div`
   align-items: center;
   min-width: 221px;
   max-width: 300px;
+
+  @media (max-width: 375px) {
+    min-width: 211px;
+    max-width: 291px;
+  }
 `;
 
 export const Image = styled.img`
   margin-right: 12px;
+
+  @media (max-width: 375px) {
+    height: 70px;
+  }
 `;
 
 export const Description = styled.p`
@@ -16,4 +25,9 @@ export const Description = styled.p`
   line-height: 20px;
   font-weight: 600;
   color: #333;
+
+  @media (max-width: 375px) {
+    font-size: 14px;
+    line-height: 18px;
+  }
 `;
--- src/components/Advantages.styles.js
+++ src/components/Advantages.styles.js
@@ -9,4 +9,13 @@ export const AdvantagesContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   gap: 80px;
+
+  @media (max-width: 375px) {
+    height: auto;
+    max-width: 335px;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 20px;
+  }
 `;
