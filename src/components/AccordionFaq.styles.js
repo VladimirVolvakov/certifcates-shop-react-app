9ff324c901c09bfe66e862b77f3a45ad282f515b
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-20 21:39:06 +0300

    style: add styles for media query < 375 px for AccordionFaq component

--- src/components/AccordionFaq.styles.js
+++ src/components/AccordionFaq.styles.js
@@ -4,6 +4,11 @@ export const Container = styled.div`
   width: 100%;
   max-width: 1300px;
   margin: 100px auto 95px;
+
+  @media (max-width: 375px) {
+    max-width: 335px;
+    margin: 60px auto;
+  }
 `;
 
 export const Title = styled.h2`
@@ -13,6 +18,12 @@ export const Title = styled.h2`
   color: #333;
   text-align: center;
   margin-bottom: 60px;
+
+  @media (max-width: 375px) {
+    font-size: 21px;
+    line-height: 25px;
+    margin-bottom: 30px;
+  }
 `;
 
 export const Accordion = styled.div``;
