f8d0e71e6289756665b86057eea5e8c3658077d2
VladimirVolvakov <dr.volvakov@gmail.com>
2023-01-25 15:12:21 +0300

    style: add styles for OrderForm component

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,5 +1,5 @@
 // Styles:
-import { Button, Checkbox, Container, Form, Header, Input, Label, PersonalData, Subheader, UnderlinedText } from "./OrderForm.styles";
+import { Button, Checkbox, Container, Form, Header, Input, Label, PersonalData, Subheader, Textarea, UnderlinedText } from "./OrderForm.styles";
 
 const OrderForm = () => {
   return (
@@ -7,9 +7,9 @@ const OrderForm = () => {
       <Header>Отправьте заявку</Header>
       <Subheader>Мы свяжемся с Вами в ближайшее время</Subheader>
       <Form>
-        <Input type="text" placeholder="Имя" />
-        <Input type="text" placeholder="Телефон" />
-        <Input type="text" placeholder="Комментарий" />
+        <Input type="text" id="name" placeholder="Имя" />
+        <Input type="tel" id="phone" placeholder="Телефон" />
+        <Textarea name="comment" id="comment" rows="6" placeholder="Комментарий" />
         <PersonalData>
           <Checkbox type="checkbox" id="data-processing" name="data-processing" />
           <Label for="data-processing">
--- src/components/OrderForm.styles.js
+++ src/components/OrderForm.styles.js
@@ -1,21 +1,100 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 530px;
+  margin: 0 auto;
+  padding: 30px;
+  border-radius: 20px;
+  background: #fff;
+  backdrop-filter: blur(25px);
+`;
 
-export const Header = styled.h3``;
+export const Header = styled.h3`
+  font-size: 30px;
+  line-height: 34px;
+  font-weight: 600;
+  color: #333;
+  margin-bottom: 15px;
+`;
 
-export const Subheader = styled.p``;
+export const Subheader = styled.p`
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #333;
+  margin-bottom: 30px;
+`;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #333;
+  padding: 20px 15px;
+  border: 1px solid #c2c2c2;
+  border-radius: 10px;
+`;
 
-export const PersonalData = styled.div``;
+export const Textarea = styled.textarea`
+  height: 155px;
+  scroll: none;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #333;
+  padding: 15px;
+  border: 1px solid #c2c2c2;
+  border-radius: 10px;
+`;
 
-export const Checkbox = styled.input``;
+export const PersonalData = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-export const Label = styled.label``;
+export const Checkbox = styled.input`
+  height: 12px;
+  width: 12px;
+  border: 1px solid #547147;
+  accent-color: #547147;
+  margin-right: 10px;
+  cursor: pointer;
+`;
 
-export const UnderlinedText = styled.span``;
+export const Label = styled.label`
+  font-size: 11px;
+  line-height: 20px;
+  font-weight: 400;
+  color: #333;
+`;
 
-export const Button = styled.button``;
+export const UnderlinedText = styled.span`
+  text-decoration: underline;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 32px;
+  border-radius: 40px;
+  border: none;
+  background: #547147;
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    background: #547900;
+    opacity: 0.8;
+  }
+`;
